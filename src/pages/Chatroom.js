757f8c4694e6c832c2457ed18b8c467e0087d46f
Atsuya Tanaka <tenmusu007@gmail.com>
2022-11-27 07:01:51 -0800

    add extraheader

--- src/pages/Chatroom.js
+++ src/pages/Chatroom.js
@@ -14,15 +14,13 @@ import Paper from '@mui/material/Paper';
 import { AuthContext } from '../AuthContext';
 import { baseUrl } from '../helper/baseUrl';
 
-const socket = io(
-	process.env.REACT_APP_SERVER_URL,
-	{
-		withCredentials: true,
+const socket = io(process.env.REACT_APP_SERVER_URL, {
+	withCredentials: true,
+	transports: ["websocket"],
+	extraHeaders: {
+		"my-custom-header": "abcd",
 	},
-	{
-		transports: ["websocket"],
-	}
-);
+});
 
 const Chatroom = () => {
   const params = useParams();
