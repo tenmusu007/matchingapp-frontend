a8639bbd57d7d6b35e07545a8ba1f8f8a12a7f49
Atsuya Tanaka <tenmusu007@gmail.com>
2022-11-28 08:37:11 -0800

    add url

--- src/state/dispatch.js
+++ src/state/dispatch.js
@@ -4,7 +4,10 @@ export const loginCall = async (user, dispatch) => {
   dispatch({ type: 'LOGIN_START' });
 
   try {
-    const res = await axios.post(`/login`, user)
+    const res = await axios.post(
+			`https://pairs-server.herokuapp.com/login`,
+			user
+		);
     dispatch({ type: 'LOGIN_SUCCESS', payload: res.data });
   } catch (err) {
     dispatch({ type: 'LOGIN_ERROR', payload: err });
