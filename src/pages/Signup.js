ca3d22ef071abf535fe65c249a5991310ff8dc7a
Koki Sakai <likuor0528@gmail.com>
2022-11-30 16:02:44 -0800

    Refactor

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useRef, useEffect } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
