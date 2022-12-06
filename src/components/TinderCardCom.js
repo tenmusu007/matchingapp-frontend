677cd9a8aad5a2a213401afa90efd2586131ae31
Koki Sakai <likuor0528@gmail.com>
2022-12-06 15:03:55 -0800

    Xif profile bug

--- src/components/TinderCardCom.js
+++ src/components/TinderCardCom.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import TinderCard from 'react-tinder-card';
 import style from 'styled-components';
 import '../styles/style.css';
