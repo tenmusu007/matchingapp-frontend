e8432b6c0ffa9248b4d7dc306470fd8d20631577
Koki Sakai <likuor0528@gmail.com>
2022-12-06 15:18:25 -0800

    Refactor

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -6,7 +6,6 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import Divider from '@mui/material/Divider';
 import { styled } from '@mui/material/styles';
 import Badge from '@mui/material/Badge';
 import Button from '@mui/material/Button';
